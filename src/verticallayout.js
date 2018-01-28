ec1fe2bb9aee0a3b0d9238b7b987a22fe91a4df7
James Baicoianu <james_github@baicoianu.com>
2018-01-27 17:38:41 -0800

    Use create() instead of createChildren()

--- src/verticallayout.js
+++ src/verticallayout.js
@@ -2,7 +2,7 @@ room.registerElement('verticallayout', {
   offset: V(0,1,0),
   spacing: 1,
   // FIXME - need proxy inheritance so we don't need to duplicate this function
-  createChildren: function() {
+  create: function() {
     var children = this.children;
     console.log('children!', children);
     var i = 0;
